3ea6de0e6f48cfc15a5c1e79a6169e937ca231e7
matteli <matthieu.nue@gmail.com>
2020-11-23 18:49:59 +0100

    8in2 amelioration

--- yesmaster/algo/compatible_8in2.py
+++ yesmaster/algo/compatible_8in2.py
@@ -7,29 +7,42 @@ def init():
     test_done = []
 
 
+def loop():
+    while True:
+        test = random_code()
+        if test_dict[test[0]][test[1]][test[2]][test[3]]["done"] == False:
+            compatible = True
+            for t in test_done:
+                good, bad = verify(test, t)
+                if (
+                    good != test_dict[t[0]][t[1]][t[2]][t[3]]["good"]
+                    or bad != test_dict[t[0]][t[1]][t[2]][t[3]]["bad"]
+                ):
+                    test_dict[test[0]][test[1]][test[2]][test[3]]["done"] = True
+                    compatible = False
+                    break
+            if compatible:
+                break
+    return test
+
+
 def get():
     global test_dict, test_done, test1
     if len(test_done) == 0:
         test = random_code_4_colors()
         test1 = test
     elif len(test_done) == 1:
-        test = random_code_4_colors(except_colors=test1)
+        t = test_done[0]
+        gb = (
+            test_dict[t[0]][t[1]][t[2]][t[3]]["good"]
+            + test_dict[t[0]][t[1]][t[2]][t[3]]["bad"]
+        )
+        if gb < 4:
+            test = random_code_4_colors(except_colors=test1)
+        else:
+            test = loop()
     else:
-        while True:
-            test = random_code()
-            if test_dict[test[0]][test[1]][test[2]][test[3]]["done"] == False:
-                compatible = True
-                for t in test_done:
-                    good, bad = verify(test, t)
-                    if (
-                        good != test_dict[t[0]][t[1]][t[2]][t[3]]["good"]
-                        or bad != test_dict[t[0]][t[1]][t[2]][t[3]]["bad"]
-                    ):
-                        test_dict[test[0]][test[1]][test[2]][test[3]]["done"] = True
-                        compatible = False
-                        break
-                if compatible:
-                    break
+        test = loop()
     return test
 
 

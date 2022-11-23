35b831c11cf908e52b489ea99c4821848444c4c4
matteli <matthieu.nue@gmail.com>
2022-11-23 17:24:55 +0100

    add a template

---
+++ yesmaster/algo/template.py
@@ -0,0 +1,11 @@
+#This file show how your algo must be organized.
+
+class Algo:
+
+    def get(self):
+        #This function is executed for suggesting a code. Must return a 4 letters string with letters among ROGBYAPW
+        return #CODE
+    
+    def report(self, test, good, bad):
+        #This function is executed executed with 3 arguments (test : the code that you pushed with get, good : the number of good colors at good place, bad : the number of good colors at wrong place). Return nothing.
+        return
